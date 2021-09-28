086bb0b616085a27a6c9744e8a3cbbfa43743c46
santisobral <ssrediciones@gmail.com>
2021-09-28 20:42:11 -0300

    ignorar modules

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const app = express();
 const publicPath= path.resolve(__dirname,'./public')
 app.use( express.static(publicPath) );
 
-app.listen(3050, ()=> {
-    console.log('Servidor corriendo en el puerto 3050')
+app.listen(process.env.PORT || 3050, ()=> {
+    console.log('Servidor corriendo en el puerto 3010')
 });
 
 app.get('/', (req,res)=> {
