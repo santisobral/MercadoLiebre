4b87b8fbb7b8cc358f8f0e0fae6fd1ce3b27fe36
santisobral <ssrediciones@gmail.com>
2021-09-28 18:46:54 -0300

    commit entrega

---
+++ app.js
@@ -0,0 +1,31 @@
+const express= require ("express");
+const path= require ("path");
+
+const app = express();
+
+const publicPath= path.resolve(__dirname,'./public')
+app.use( express.static(publicPath) );
+
+app.listen(3050, ()=> {
+    console.log('Servidor corriendo en el puerto 3050')
+});
+
+app.get('/', (req,res)=> {
+    res.sendFile(path.resolve(__dirname,'./views/home.html'));
+});
+
+app.get('/register', (req,res)=> {
+    res.sendFile(path.resolve(__dirname,'./views/register.html'));
+});
+
+app.post('/registro', (req,res)=> {
+    res.redirect('/');
+});
+
+app.get('/login', (req,res)=> {
+    res.sendFile(path.resolve(__dirname,'./views/login.html'));
+});
+
+app.post('/login', (req,res)=> {
+    res.redirect('/');
+});
